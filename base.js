59f35582f06276711fc7981beba141238879bfee
JhoneLee <a541606744@126.com>
2014-05-11 17:16:58 +0800

    新增事件绑定 、返回元素内elemnt类型元素集合、中文nodeType提示

--- base.js
+++ base.js
@@ -1,5 +1,39 @@
 // JavaScript Document
 //**********DOM操作*****************
+//返回通俗的Node类型
+function getNodeType(obj)
+{
+	var myNodeType={
+	 '1':'element对象,nodeValue=null,nodeName=标签名（大写）',
+	 '2':'属性对象,nodeValue=属性值,nodeName=属性名称（大写）',
+	 '3':'文本节点,nodeValue=文本,nodeName=#text',
+	 '4':'XML CDATA区域,nodeValue=区域内容,nodeName=#cdata-section',
+	 '5':'XML实体引用',
+	 '6':'XML实体',
+	 '7':'XML处理指令',
+	 '8':'注释节点,nodeValue=注释的内容,nodeName=#comment',
+	 '9':'document文档类型,nodeValue=null,nodeName=#document',
+	 '10':'docType类型,nodeValue=null,nodeName=doctype内容',
+	 '11':'文档片段,nodeValue=null,nodeName=#document-fragment',
+	 '12':'XML中非 XML 数据的格式'
+	};
+	return myNodeType[obj.nodeType];
+}
+//获取元素中element节点集合
+function getChildNodes(obj)
+{
+	var arr=[];
+	var son=obj.childNodes;
+	var len=son.length;
+	for(var i=0;i<len;i++)
+	{
+		if(son[i].nodeType=='1')
+		{
+			arr.push(son[i]);
+		}
+	}
+	return arr;
+}
 //返回元素的样式值
 function getStyle(obj,styleName)
 {
@@ -36,6 +70,35 @@ function getPosition(obj)
 		obj=obj.parentNode;
 	}
 	return pos;
+}
+//************DOM事件*****************
+//事件绑定对象
+function addEvent(obj,type,handle)
+{
+   
+  if(obj.addEventListener)
+  {
+	  obj[type+handle]=handle;
+	  obj.addEventListener(type,obj[type+handle],false);
+  }
+  else
+  {
+	  obj[type+handle]=function(){handle.call(obj);};
+	  obj.attachEvent('on'+type,obj[type+handle]);  
+  }
+}
+//取消事件绑定
+function removeEvent(obj,type,handle)
+{
+	var hand=obj[type+handle];
+	if(obj.removeEventListener)
+	{
+      obj.removeEventListener(type,hand,false);	  	
+	}
+	else
+	{
+		obj.detachEvent('on'+type,hand);
+	}
 }
 //*********Ajax********************
 //创建xhr对象
